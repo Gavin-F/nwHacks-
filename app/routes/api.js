698fb2c0235b819383aa63818da5efed1dcf31fa
George <georgegao96@hotmail.com>
2016-02-27 22:22:58 -0800

    jobtitles api now returns distinct list

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,36 +3,24 @@ var router = express.Router();
 var JobPost = require("../models/jobpost.js");
 var UserReview = require("../models/userreview.js");
 
-//router.get("/jobtitles", function(req, res) {
-//    // Placeholder
-//    //var jobtitles = ["application developer", "software engineer", "financial analyst"];
-//    //res.json(jobtitles);
-
-//
-//});
-//
-//router.get("/companies", function(req, res){
-//
-//});
-//
-//router.get("/schools", function(req, res) {
-//
-//});
-//
-//router.get("/faculties", function(req, res) {
-//
-//});
-
 router.get("/jobtitles", function(req, res) {
-    JobPost.find().distinct({}, function(err, docs) {
-        var json_res = [];
-        for (var i=0; i<docs.length; i++) {
-            json_res.push(docs[i]["jobTitle"]);
-        }
-
-        res.json(json_res);
-    });
+    JobPost.find().distinct("jobTitle", function(err, results) {
+        res.json(results);
+    })
 });
+
+router.get("/companies", function(req, res){
+
+});
+
+router.get("/schools", function(req, res) {
+
+});
+
+router.get("/faculties", function(req, res) {
+
+});
+
 module.exports = router;
 
 // list of job title
